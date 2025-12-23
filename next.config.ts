d5434d4562a4ca1a9e2bec8c43ab774c351e9b46
Syahridwan <169109865+SyahReedOne@users.noreply.github.com>
2025-12-23 18:46:42 +0800

    A small tweak

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export', // important for static export
-  basePath: isProd ? '/your-repo-name' : '',
-  assetPrefix: isProd ? '/your-repo-name/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
 }
 
 module.exports = nextConfig
